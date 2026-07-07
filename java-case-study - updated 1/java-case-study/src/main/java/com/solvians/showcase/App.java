bfd02bc3d6c7113c9c94414b6dda820030b26aa8
Nilima <pawar.nilima11@gmail.com>
2026-07-07 12:48:37 +0100

    Fixed App.java to print the lines of generated certificate updates.

--- java-case-study - updated 1/java-case-study/src/main/java/com/solvians/showcase/App.java
+++ java-case-study - updated 1/java-case-study/src/main/java/com/solvians/showcase/App.java
@@ -1,5 +1,7 @@
 package com.solvians.showcase;
 
+import java.util.Arrays;
+
 /**
  * Hello world!
  */
@@ -9,13 +11,15 @@ public class App {
     }
 
     public static void main(String[] args) {
-        if (args.length >= 2) {
-            int threads = Integer.parseInt(args[0]);
-            int quotes = Integer.parseInt(args[1]);
-
-            CertificateUpdateGenerator certificateUpdateGenerator = new CertificateUpdateGenerator(threads, quotes);
-            certificateUpdateGenerator.generateQuotes();
+        if (args.length < 2) {
+            throw new RuntimeException("Expect at least number of threads and number of quotes. But got: " + Arrays.toString(args));
         }
-        throw new RuntimeException("Expect at least number of threads and number of quotes. But got: " + args);
+
+        int threads = Integer.parseInt(args[0]);
+        int quotes = Integer.parseInt(args[1]);
+
+        CertificateUpdateGenerator certificateUpdateGenerator = new CertificateUpdateGenerator(threads, quotes);
+        certificateUpdateGenerator.generateQuotes()
+                .forEach(System.out::println);
     }
 }
